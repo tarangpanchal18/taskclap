35d8def29649cb04a27da89e3a74b01d24b6c553
wiswebapp <tarang.webinfosolutions@gmail.com>
2023-04-28 00:20:28 +0530

    added passport api for category-subcategory

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
          * Package Service Providers...
          */
         Yajra\DataTables\DataTablesServiceProvider::class,
+        Laravel\Passport\PassportServiceProvider::class,
 
         /*
          * Application Service Providers...
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CategoryApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('category', [CategoryApiController::class, 'index'])->name('api.category');
+Route::get('subcategory', [CategoryApiController::class, 'subcategory'])->name('api.subcategory');
