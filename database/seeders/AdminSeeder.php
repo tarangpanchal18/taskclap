8e34b2a458f772c22e617ef441311aabe1d6d369
Tarang Panchal <tarang.codexalters@gmail.com>
2023-04-27 11:18:00 +0530

    added admin data seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Admin::create([
+			'name' => 'Super Admin',
+			'email' => 'demo@demo.com',
+			'password' => Hash::make('123456'),
+		]);
+    }
+}
