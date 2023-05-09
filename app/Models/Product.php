2eab137764b3d8bc49021459cfdb6ee32a6fc227
Tarang Panchal <tarang.codexalters@gmail.com>
2023-05-09 13:29:36 +0530

    add: relationships for parent child product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
@@ -13,18 +14,27 @@ class Product extends Model
 
     const UPLOAD_PATH = "uploads/products";
 
+    const SERVICE_TYPE = [
+        'Service',
+        'Repair',
+        'InstallUninstall',
+    ];
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
     protected $fillable = [
+        'parent_id',
         'category_id',
         'sub_category_id',
+        'service_type',
         'title',
         'description',
         'long_description',
         'image',
+        'strike_price',
         'price',
         'commission',
         'approx_duration',
@@ -39,5 +49,15 @@ class Product extends Model
     public function subCategory(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(Product::class , 'parent_id');
+    }
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Product::class , 'parent_id');
     }
 }
