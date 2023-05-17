63b20f0959e31bcfd5d7c8a61e4f8488c264d7b7
Tarang Panchal <tarang.codexalters@gmail.com>
2023-05-17 12:23:25 +0530

    add: new fields in provider module

--- app/Http/Requests/ProviderRequest.php
+++ app/Http/Requests/ProviderRequest.php
@@ -26,14 +26,17 @@ class ProviderRequest extends FormRequest
     {
 
         $rule = [
+            'category_id' => 'required|array',
             'name' => 'required|min:3|max:100',
             'email' => [
                 'nullable', 'min:3', 'max:100', 'email:rfc', Rule::unique('providers', 'email')->ignore($this->provider->id)
             ],
-            'phone' => 'required|numeric|digits_between:9,12',
+            'phone' => 'required|numeric|digits_between:9,10',
             'address' => 'nullable|min:10|max:1000',
             'image' => 'nullable|mimes:jpg,jpeg,png,gif|max:5120',
-            'notes' => 'nullable|min:10|max:1000',
+            'id_proof' => 'nullable|mimes:jpg,jpeg,png,gif|max:5120',
+            'notes' => 'nullable|min:3|max:1000',
+            'vehicle_number' => 'nullable|min:10|max:50',
             'status' => 'required',
         ];
 
--- app/Models/Provider.php
+++ app/Models/Provider.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -10,12 +11,26 @@ class Provider extends Model
 {
     use HasFactory, SoftDeletes;
 
+    const UPLOAD_PATH = "uploads/provider";
+    const UPLOAD_PATH_DOC = "uploads/provider/documents";
+
+    protected function categoryId(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => json_decode($value, true),
+            set: fn ($value) => json_encode($value),
+        );
+    }
+
     protected $fillable = [
+        'category_id',
         'name',
         'email',
         'phone',
         'address',
         'image',
+        'id_proof',
+        'vehicle_number',
         'notes',
         'status',
     ];
