ec43c9371c8abc358613198fc436e207db9e0c58
wiswebapp <tarang.webinfosolutions@gmail.com>
2023-05-31 23:52:18 +0530

    add auth login using firebase

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,94 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
-</head>
-<body>
-    <h1>Frontend Login Page</h1>
-    <hr>
-    <form action="{{ route('login') }}" method="POST">
-        @csrf
-        <input type="text" name="email" placeholder="Enter Email" required>
-        <input type="password" name="password" placeholder="Enter Password" required>
-        <input type="submit" value="Login">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    </form>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container mt-5" style="max-width: 550px">
+        <div class="alert alert-danger" id="error" style="display: none;"></div>
+        <h3>Please Login to continue</h3>
+        <div class="alert alert-success" id="successAuth" style="display: none;"></div>
+        <form>
+            <label>Phone Number:</label>
+            <input type="text" id="number" class="form-control" placeholder="+91 ********" value="+91 7383742776">
+            <div id="recaptcha-container"></div>
+            <button type="button" class="btn btn-primary mt-3" onclick="sendOTP();">Send OTP</button>
+        </form>
+
+        <div class="mb-5 mt-5">
+            <h3>Add verification code</h3>
+            <div class="alert alert-success" id="successOtpAuth" style="display: none;"></div>
+            <form>
+                <input type="text" id="verification" class="form-control" placeholder="Verification code" value="123456">
+                <button type="button" class="btn btn-danger mt-3" onclick="verify()">Verify code</button>
+            </form>
+        </div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/6.0.2/firebase.js"></script>
+    <script>
+        var firebaseConfig = {
+            apiKey: "AIzaSyB8J5VxtjPFjjFB54QbQ0p8wLfMnZwdH0w",
+            authDomain: "taskclap-2023.firebaseapp.com",
+            databaseURL: "https://taskclap-2023.firebaseio.com",
+            projectId: "taskclap-2023",
+            storageBucket: "taskclap-2023.appspot.com",
+            messagingSenderId: "380954429436",
+            appId: "1:380954429436:web:fed0c4bb3e2215ef4d5479"
+        };
+        firebase.initializeApp(firebaseConfig);
+    </script>
+    <script type="text/javascript">
+        window.onload = function () {
+            render();
+        };
+        function render() {
+            window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+            recaptchaVerifier.render();
+        }
+        function sendOTP() {
+            var number = $("#number").val();
+            firebase.auth().signInWithPhoneNumber(number, window.recaptchaVerifier).then(function (confirmationResult) {
+                window.confirmationResult = confirmationResult;
+                coderesult = confirmationResult;
+                console.log(coderesult);
+                $("#successAuth").text("Message sent");
+                $("#successAuth").show();
+            }).catch(function (error) {
+                $("#error").text(error.message);
+                $("#error").show();
+            });
+        }
+        function verify() {
+            var code = $("#verification").val();
+            coderesult.confirm(code).then(function (result) {
+                var user = result.user;
+                console.log(user);
+                $("#successOtpAuth").text("Auth is successful");
+                $("#successOtpAuth").show();
+                $.ajax({
+                    type : "POST",
+                    url : "{{ route('login') }}",
+                    data : {
+                        '_token': "{{ csrf_token() }}",
+                        'phone' : $("#number").val()
+                    },
+                    success : function(response) {
+                        window.location.href = '/dashboard';
+                    }
+                });
+            }).catch(function (error) {
+                $("#error").text(error.message);
+                $("#error").show();
+            });
+        }
+    </script>
 </body>
+
 </html>
