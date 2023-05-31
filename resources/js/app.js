b99ecdc00d0014500810c26ea1a11cba91c9e4d7
Tarang Panchal <tarang.webinfosolutions@gmail.com>
2023-05-31 19:02:20 +0530

    add vue js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,15 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import { createApp } from 'vue'
+import Welcome from './components/Welcome.vue'
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const app = createApp({})
+
+app.component('welcome', Welcome)
+
+app.mount('#app')
