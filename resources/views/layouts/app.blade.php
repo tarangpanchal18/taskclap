f9fdbb270df74e21a9089eb4570ceeed3d3b14ca
Tarang Panchal <tarang.webinfosolutions@gmail.com>
2023-06-02 17:17:55 +0530

    added basic files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,20 +1,124 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="https://bootswatch.com/5/lux/bootstrap.min.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-    </head>
-    <body>
-        @include('layouts.headerbar')
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+	<title>{{ config('app.name', 'Laravel') }}</title>
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="assets/img/favicon-01.png">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<!-- Fontawesome CSS -->
+	<link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+	<link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+	<!-- Fearther CSS -->
+	<link rel="stylesheet" href="assets/css/feather.css">
+	<!-- select CSS -->
+	<link rel="stylesheet" href="assets/plugins/select2/css/select2.min.css">
+	<!-- Owl carousel CSS -->
+	<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+	<!-- Datepicker CSS -->
+	<link rel="stylesheet" href="assets/css/bootstrap-datetimepicker.min.css">
+	<!-- Aos CSS -->
+	<link rel="stylesheet" href="assets/plugins/aos/aos.css">
+	<!-- Main CSS -->
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
 
-        <div id="app">
-            @yield('section')
-        </div>
+<body class="home-page-three">
+	<div class="main-wrapper">
 
-        <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
-        <script src="{{ mix('js/app.js') }}"></script>
-        @yield('js')
-    </body>
+		<!-- Header -->
+		@include('layouts.header')
+		<!-- /Header -->
+
+		<!-- Banner Section -->
+        @include('layouts.home-banner')
+		<!-- /Banner Section -->
+
+        @yield('content')
+
+        <!-- Footer -->
+        @include('layouts.footer')
+        <!-- /Footer -->
+
+		<!-- Cursor -->
+		<div class="mouse-cursor cursor-outer"></div>
+		<div class="mouse-cursor cursor-inner"></div>
+		<!-- /Cursor -->
+
+		<!-- Delete Account -->
+		<div class="modal fade custom-modal" id="del-account">
+			<div class="modal-dialog modal-dialog-centered">
+				<div class="modal-content">
+					<div class="modal-header border-bottom-0 justify-content-between">
+						<h5 class="modal-title">Delete Account</h5>
+						<button type="button" class="close-btn" data-bs-dismiss="modal" aria-label="Close"><i class="feather-x"></i></button>
+					</div>
+					<div class="modal-body pt-0">
+						<div class="write-review">
+							<form action="login.html">
+								<p>Are you sureyou want to delete This Account? To delete your account, Type your password.</p>
+								<div class="form-group">
+									<label class="col-form-label">Password</label>
+									<div class="pass-group">
+										<input type="password" class="form-control pass-input" placeholder="*************">
+										<span class="toggle-password feather-eye"></span>
+									</div>
+								</div>
+								<div class="modal-submit text-end">
+									<a href="#" class="btn btn-secondary me-2" data-bs-dismiss="modal">Cancel</a>
+									<button type="submit" class="btn btn-danger">Delete Account</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- /Delete Account -->
+
+	</div>
+
+
+	<!-- scrollToTop start -->
+	<div class="progress-wrap active-progress progress-wrap-three">
+		<svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+		<path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" style="transition: stroke-dashoffset 10ms linear 0s; stroke-dasharray: 307.919px, 307.919px; stroke-dashoffset: 228.265px;"></path>
+		</svg>
+	</div>
+	<!-- scrollToTop end -->
+
+	<!-- jQuery -->
+	<script src="assets/js/jquery-3.6.1.min.js"></script>
+
+	<!-- Bootstrap Core JS -->
+	<script src="assets/js/bootstrap.bundle.min.js"></script>
+
+	<!-- Fearther JS -->
+	<script src="assets/js/feather.min.js"></script>
+
+	<!-- Owl Carousel JS -->
+	<script src="assets/js/owl.carousel.min.js"></script>
+
+	<!-- select JS -->
+	<script src="assets/plugins/select2/js/select2.min.js"></script>
+
+	<!-- Datepicker Core JS -->
+	<script src="assets/js/moment.min.js"></script>
+	<script src="assets/js/bootstrap-datetimepicker.min.js"></script>
+
+	<!-- Aos -->
+	<script src="assets/plugins/aos/aos.js"></script>
+
+	<!-- Slick JS -->
+	<script src="assets/js/slick.js"></script>
+
+	<!-- Top JS -->
+	<script src="assets/js/backToTop.js"></script>
+
+	<!-- Custom JS -->
+	<script src="assets/js/script.js"></script>
+
+</body>
 </html>
---
+++ resources/views/layouts/home-banner.blade.php
@@ -0,0 +1,33 @@
+<section class="hero-section-three" style="background: url('https://res.cloudinary.com/urbanclap/image/upload/images/growth/home-screen/1615375782838-f890f8.jpeg');background-repeat: no-repeat;background-size: cover;background-position: center;background-color: #00000073; background-blend-mode: multiply;">
+    <div class="container">
+        <div class="home-banner home-banner-three aos" data-aos="fade-up">
+            <div class="row align-items-center w-100">
+                <div class="col-lg-6 col-md-12 mx-auto">
+                    <div class="section-search section-section-three">
+                        <h4>Best Saloon Template</h4>
+                        <h1>Find the Awesome Saloo's Near you</h1>
+                        <p>Hair and SPA Salons and fully integrated with theme tools that you can use for the promotion of your business.</p>
+                        <a  href="javascript:void(0);" class="make-appointment">MAKE AN APPOINTMENT</a>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                </div>
+            </div>
+        </div>
+        <div class="search-box search-box-three aos" data-aos="fade-up">
+            <form action="listing-grid-sidebar.html" class="d-flex search-form-profile">
+                <div class="searchbox-list">
+                    <div class="search-input search-input-three">
+                        <i class="feather-map-pin me-2"></i>
+                        <div class="form-group mb-0">
+                            <input class="form-control" type="text" placeholder="Location">
+                        </div>
+                    </div>
+                </div>
+                <div class="search-btn search-btn-three">
+                    <button href="#" class="btn btn-primary" type="submit"><i class="feather-search me-2"></i>SEARCH</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
