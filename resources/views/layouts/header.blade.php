898f17153a22753d7b17153f0ec7f344ae85c3fa
wiswebapp <tarang.webinfosolutions@gmail.com>
2023-06-04 23:22:31 +0530

    wip

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -10,44 +10,64 @@
                     </span>
                 </a>
                 <a href="index.html" class="navbar-brand logo">
-                    <img src="assets/img/logo-02.svg" class="img-fluid" alt="Logo">
+                    <img src="assets/img/taskclap-logo.svg" class="img-fluid" alt="Logo">
                 </a>
                 <a href="index.html" class="navbar-brand logo-small">
-                    <img src="assets/img/logo-icon.png" class="img-fluid" alt="Logo">
+                    <img src="assets/img/taskclap-logo.svg" class="img-fluid" alt="Logo">
                 </a>
             </div>
             <div class="main-menu-wrapper">
                 <div class="menu-header">
                     <a href="index.html" class="menu-logo">
-                        <img src="assets/img/logo-02.svg" class="img-fluid" alt="Logo">
+                        <img src="assets/img/taskclap-logo.svg" class="img-fluid" alt="Logo">
                     </a>
                     <a id="menu_close" class="menu-close" href="javascript:void(0);"> <i class="fas fa-times"></i></a>
                 </div>
                 <ul class="main-nav">
                     <li><a href="{{ route('homepage') }}">Home</a></li>
                     <li><a href="{{ route('homepage') }}">Ac/Appliances Service</a></li>
-                    <li><a href="{{ route('homepage') }}">Salon For Men</a></li>
-                    <li><a href="{{ route('homepage') }}">Salon For Women</a></li>
                     <li><a href="{{ route('homepage') }}">Home Painting</a></li>
                     <li><a href="{{ route('homepage') }}">Cleaning & Pesting</a></li>
                     <li class="login-link">
                         <a href="choose-signup.html">Register As Provider</a>
                     </li>
+                    @if (Auth::guest())
                     <li class="login-link">
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
                     </li>
+                    @else
+                    <div class="cta-btn" style="min-width: 100%">
+                        <a class="btn" href="{{ route('logout') }}"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i
+                                class="feather-log-out me-2"></i> Logout</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </div>
+                    @endif
                 </ul>
             </div>
             <ul class="nav header-navbar-rht">
+                @if (Auth::guest())
                 <li class="nav-item">
                     <a class="nav-link header-login" href="choose-signup.html"><i class="feather-calendar me-2"></i>Register As Provider</a>
                 </li>
+                @endif
                 <li class="nav-item">
+                    @if (Auth::guest())
                     <div class="cta-btn">
                         <a class="btn" href="provider-signup.html"><i class="feather-users me-2"></i>REGISTER /</a>
                         <a class="btn ms-1" href="{{ route('login') }}">LOGIN</a>
                     </div>
+                    @else
+                    <div class="cta-btn" style="min-width: 100%">
+                        <a class="btn" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="feather-log-out me-2"></i> Logout</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </div>
+                    @endif
                 </li>
             </ul>
         </nav>
--- routes/auth.php
+++ routes/auth.php
@@ -12,31 +12,23 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-                ->name('register');
-
+    Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
     Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
-
     Route::post('login', [AuthenticatedSessionController::class, 'storeWithOtp']);
+    Route::get('v1/login', [AuthenticatedSessionController::class, 'create_v1'])->name('login.v1');
+    Route::post('v1/login', [AuthenticatedSessionController::class, 'storeWithOtp_v1']);
 
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-                ->name('password.request');
+    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
+    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
 
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-                ->name('password.email');
-
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-                ->name('password.reset');
-
-    Route::post('reset-password', [NewPasswordController::class, 'store'])
-                ->name('password.store');
+    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
+    Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('verify-email', EmailVerificationPromptController::class)
-                ->name('verification.notice');
+    Route::get('verify-email', EmailVerificationPromptController::class)->name('verification.notice');
 
     Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
                 ->middleware(['signed', 'throttle:6,1'])
@@ -46,13 +38,11 @@ Route::middleware('auth')->group(function () {
                 ->middleware('throttle:6,1')
                 ->name('verification.send');
 
-    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-                ->name('password.confirm');
+    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])->name('password.confirm');
 
     Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->name('logout');
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
