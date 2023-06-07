4040874b251125c5e88e3fb3bded0a73565d756a
wiswebapp <tarang.webinfosolutions@gmail.com>
2023-06-07 23:54:48 +0530

    add detail in product detail page

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -19,9 +19,9 @@ class CartController extends Controller
         $category = $this->categoryRepository->getById($subcategory, true);
         foreach($category->product as $product) {
             if (! empty($product->serviceCategory->name))
-                $data[$product->serviceCategory->name] = $product;
+                $data[$product->serviceCategory->name][] = $product;
         }
-
+        // dd($data);
         return view('cart', [
             'category' => $category,
             'pageData' => $data,
