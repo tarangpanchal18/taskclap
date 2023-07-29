c876f0dcc19c6e0bc0dc11740f823f126754109e
Tarang Panchal <tarang.webinfosolutions@gmail.com>
2023-07-29 15:48:55 +0530

    wip: rating scren

---
+++ database/migrations/2023_07_29_060334_add_rating_field_to_product_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->float('rating')->after('image')->default('0.0');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('rating');
+        });
+    }
+};
