6df6ae1df45091f481de69ef0dda000325d79d68
wiswebapp <tarang.webinfosolutions@gmail.com>
2023-08-08 22:47:33 +0530

    add: live chat

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -30,3 +30,14 @@
 <!-- Custom JS -->
 <script src="/assets/js/script.js"></script>
 <script src="/assets/js/taskclapscript.js"></script>
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/{{ env("LIVECHAT_KEY") }}/{{ env("LIVECHAT_HASH") }}';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
