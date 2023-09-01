af7c7b5b05522aba00d7b086caf4b8520850454e
Tarang Panchal <tarang.webinfosolutions@gmail.com>
2023-09-01 15:58:58 +0530

    fix: homeData api image path

--- app/Http/Controllers/Api/v1/HomeApiController.php
+++ app/Http/Controllers/Api/v1/HomeApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API\v1;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController;
+use App\Models\Category;
 use App\Repositories\Admin\CategoryRepository;
 
 class HomeApiController extends BaseController
@@ -15,11 +16,17 @@ class HomeApiController extends BaseController
     public function getHomeData()
     {
         $fieldToGet =  ['id', 'name', 'description', 'image'];
-        $categories = $this->categoryRepository->getRaw([
-            'status' => 'Active',
-            'parent_id' => null
-        ])->select($fieldToGet)->get();
-        $categories = $categories->reject(function ($category) {
+        $urlPrefix = asset('storage' . DIRECTORY_SEPARATOR  . Category::UPLOAD_PATH . DIRECTORY_SEPARATOR );
+
+        $categories = $this->categoryRepository->getRaw(['status' => 'Active','parent_id' => null])->select($fieldToGet)->get();
+        $categories = $categories->map(function ($row) use($urlPrefix) {
+            $row->image = $urlPrefix . $row->image;
+            $row->children->map(function ($child, $key) use($row, $urlPrefix) {
+                $counter = $key + $counter;
+                $row->children[$counter]->image = $urlPrefix . $child->image;
+            });
+            return $row;
+        })->reject(function ($category) {
             return $category->children->count() == 0;
         });
         $returnArr['category'] = array_values($categories->toArray());
